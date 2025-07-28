8a24c0244ac53a14c873f0f082398136968d675a
hrvoje12345 <hrvoje.kukolj@porsche.digital>
2025-07-28 19:14:47 +0200

    feat: add initial routes for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      Initial
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/login" element={<div>login</div>} />
+          <Route path="*" element={<div>No page</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
+
   );
 }
 
