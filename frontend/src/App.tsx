f3242f027e92854b6a5af288f6dad603d7d9c629
hrvoje12345 <hrvoje.kukolj@porsche.digital>
2025-07-28 20:35:39 +0200

    feat: initial oauth check

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+
 
 function App() {
+  const {REACT_APP_GOOGLE_CLIENT_ID} = process.env
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div>Home</div>} />
-          <Route path="/login" element={<div>login</div>} />
-          <Route path="*" element={<div>No page</div>} />
-        </Routes>
-      </BrowserRouter>
+      <GoogleOAuthProvider clientId={REACT_APP_GOOGLE_CLIENT_ID!}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>Home</div>} />
+            <Route path="/login" element={
+              <div>
+                <GoogleLogin
+                  onSuccess={credentialResponse => {
+                    console.log(credentialResponse);
+                  }}
+                  onError={() => {
+                    console.log('Login Failed');
+                  }}
+                />;
+              </div>
+            } />
+            <Route path="*" element={<div>No page</div>} />
+          </Routes>
+        </BrowserRouter>
+      </GoogleOAuthProvider>
     </div>
 
   );
