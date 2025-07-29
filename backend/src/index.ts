445ca387e83525ea9e1cefb914e42dbd060558a7
hrvoje12345 <hrvoje.kukolj@porsche.digital>
2025-07-29 11:25:06 +0200

    refactor: adjust to event domain, google Oauth and db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,47 +1,29 @@
 import express from 'express';
-import passport from 'passport';
-import { Strategy } from 'passport-google-oauth20';
 
+import { googleOAuth } from './auth/googleOAuth';
+import { eventsController } from './domains/events/events.controller';
+import { initializeDbCLient } from './db/client';
 import { getEnv } from './utils/getEnv';
 import { EnvVariable } from './values/EnvVariable';
-import { Path } from './values/Path';
 
-const port = getEnv(EnvVariable.GoogleClientId, true) || 3003;
+import 'dotenv/config';
 
-passport.use(
-  new Strategy(
-    {
-      clientID: getEnv(EnvVariable.GoogleClientId),
-      clientSecret: getEnv(EnvVariable.GoogleClientSecret),
-      callbackURL: `${getEnv(EnvVariable.BackendUrl)}${Path.AuthGoogleCallback}`,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      try {
-        console.log({ accessToken, refreshToken, profile });
+const port = getEnv(EnvVariable.Port, true) || 8080;
 
-        // STORE USERS
-
-        return done(null, { user:'todo' });
-      } catch (error) {
-        return done(error, false);
-      }
-    }
-  )
-);
+initializeDbCLient();
 
 const app = express();
 
-app.get(Path.AuthGoogle, passport.authenticate('google', { scope: ['profile'] }));
-app.get(Path.AuthGoogleCallback, 
-  passport.authenticate('google', { failureRedirect: `${getEnv(EnvVariable.FrontenddUrl)}/login` }),
-  (request, response) => {
-    response.redirect('/') 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+googleOAuth(app);
+eventsController(app);
+
+app.listen(port, (error) => {
+  if (error) {
+    throw error;
   }
-);
 
-app.get(Path.Events, () => {});
-app.post(Path.Events, () => {});
-
-app.listen(port, () => {
   console.log(`Server running at ${getEnv(EnvVariable.BackendUrl)}`);
 });
